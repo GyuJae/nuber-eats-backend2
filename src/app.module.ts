cedce980e5e749ef5ef007bf4833836c036ba0ab
Gyujae <wjdrbwo1206@naver.com>
2021-07-19 21:23:20 +0900

    introspection add

--- src/app.module.ts
+++ src/app.module.ts
@@ -71,6 +71,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     GraphQLModule.forRoot({
       // playground: process.env.NODE_ENV !== 'production',
       playground: true,
+      introspection: true,
       installSubscriptionHandlers: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       context: ({ req, connection }) => {
