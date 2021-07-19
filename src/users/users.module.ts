491bb9493e5aaf3eb7341428f9230552b4a5b9bd
Gyujae <wjdrbwo1206@naver.com>
2021-07-19 17:48:56 +0900

    userModule add Restaurant respository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from './entities/users.entity';
 import { Verification } from './entities/verification.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Verification])],
+  imports: [TypeOrmModule.forFeature([User, Verification, Restaurant])],
   providers: [UsersResolver, UsersService],
   exports: [UsersService],
 })
